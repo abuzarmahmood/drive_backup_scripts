0a9506aa14a3f5c79b3e9409f5a9cd819013825d
abuzarmahmood <abuzarmahmood@gmail.com>
2021-05-17 16:31:13 -0400

    Code to run all operations on single directory

---
+++ create_archives_pipeline.sh
@@ -0,0 +1,11 @@
+if [[ $# -ne 2 ]]; then
+    echo "Illegal number of parameters"
+    exit 2
+fi
+
+DIR=$1
+ADDR=$2
+bash cleanup_names.sh $DIR
+bash move_unclaimed_files.sh $DIR
+bash gen_file_lists.sh $DIR
+bash create_archives.sh $DIR $ADDR
