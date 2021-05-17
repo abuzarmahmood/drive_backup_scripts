b97607ffc25a80694377bdfe63286c693b77229d
abuzarmahmood <abuzarmahmood@gmail.com>
2021-05-17 16:57:42 -0400

    Use zip instead of tar, with inbuilt splitting

--- create_archives.sh
+++ create_archives.sh
@@ -11,8 +11,10 @@ mkdir $ARC_DIR
 for SUBDIR in $(find $DIR -mindepth 1 -maxdepth 1 -type d);
 do
     #echo $SUBDIR;
-    tar --use-compress-program="pigz -0 -p16" \
-        -cvf $ARC_DIR/$(basename $SUBDIR).tar.gz -C $SUBDIR .;
+    #tar --use-compress-program="pigz -0 -p16" \
+    #    -cvf $ARC_DIR/$(basename $SUBDIR).tar.gz -C $SUBDIR .;
+    cd $DIR
+    zip -r -s 100g -0 $ARC_DIR/$(basename $SUBDIR).zip $(basename $SUBDIR);
 done
 echo "$(basename $DIR) archiving complete" \
     | mail -s "Archiving folder complete" $EMAIL 
