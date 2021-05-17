dc805c8a98832fd36dbb8feeb2fa892bf14157ef
abuzarmahmood <abuzarmahmood@gmail.com>
2021-05-17 17:31:59 -0400

    Generate par files for each zip file and save appropriately

--- create_archives_pipeline.sh
+++ create_archives_pipeline.sh
@@ -9,3 +9,4 @@ bash cleanup_names.sh $DIR
 bash move_unclaimed_files.sh $DIR
 bash create_archives.sh $DIR $ADDR
 bash gen_file_lists.sh $DIR
+bash create_par_files.sh $DIR $ADDR
---
+++ create_par_files.sh
@@ -0,0 +1,17 @@
+if [[ $# -ne 2 ]]; then
+    echo "Illegal number of parameters"
+    exit 2
+fi
+
+DIR=$(realpath $1)
+EMAIL=$2
+ARC_DIR=$(dirname $DIR)/$(basename $DIR)_archive
+for SUBDIR in $(find $ARC_DIR -mindepth 1 -maxdepth 1 -iname "*zip");
+do
+    PAR_DIR="${SUBDIR:0:-4}_par2" 
+    mkdir $PAR_DIR
+    par2create -r10 -n5 $SUBDIR
+    mv $SUBDIR.* $PAR_DIR
+ done
+echo "$(basename $DIR) par-ing complete" \
+    | mail -s "$(basename $DIR) par-ing complete" $EMAIL 
