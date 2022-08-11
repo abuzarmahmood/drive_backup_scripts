1d45318e4f5deb2e3f7a2257c4198ce067afdca7
abuzarmahmood <abuzarmahmood@gmail.com>
2022-08-11 11:44:57 -0400

    Improve path specification

--- gen_file_lists.sh
+++ gen_file_lists.sh
@@ -1,5 +1,6 @@
 DIR=$1
-EMAIL=$2
-ARC_DIR="${DIR}_archive"
+#ARC_DIR="${DIR}_archive"
+ARC_DIR=$(realpath $DIR)_archive
+echo $ARC_DIR
 tree $DIR > $ARC_DIR/$(basename $DIR).tree
 find $DIR > $ARC_DIR/$(basename $DIR).list
